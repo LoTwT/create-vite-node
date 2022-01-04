67b0f54f52f576bfd00d9e4bb69d4a8110dd6c35
LoTwT <709937065@qq.com>
2022-01-04 11:03:53 +0800

    feat(create-vite-node): add .env

--- templates/vite-node/vite.config.ts
+++ templates/vite-node/vite.config.ts
@@ -1,23 +1,37 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import { VitePluginNode } from "vite-plugin-node"
 import path from "path"
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig((config) => {
+  const { mode } = config
+
+  // .env
+  const root = process.cwd()
+  const { VITE_SERVER_PORT } = loadEnv(mode, root) as unknown as ImportMetaEnv
+
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  optimizeDeps: {
-    include: [],
-  },
-  server: {
-    port: 3000,
-  },
-  plugins: [
-    ...VitePluginNode({
-      adapter: "koa",
-      appPath: "./app.ts",
-    }),
-  ],
+    optimizeDeps: {
+      include: [],
+    },
+    server: {
+      port: VITE_SERVER_PORT || 3000,
+    },
+    plugins: [
+      ...VitePluginNode({
+        adapter: "koa",
+        appPath: "./app.ts",
+      }),
+    ],
+  }
 })
+
+// 环境变量
+// https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
+interface ImportMetaEnv {
+  readonly VITE_SERVER_PORT: number
+}
